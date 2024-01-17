96377416478eb21e1134b54d78cdad2cdd5a1a42
Agustin Dev <agustinfruni@gmail.com>
2024-01-17 02:11:37 -0300

    changed resources directory and added buttons

--- src/static/gres.ts
+++ src/static/gres.ts
@@ -4,22 +4,22 @@ export const gres: iGRES[] = [
   {
     title: 'Usd',
     category: 'Icon',
-    route: '/library/usd-circulo.png',
+    route: '/src/library/usd-circulo.png',
   },
   {
     title: 'Telegram',
     category: 'Icon',
-    route: '/library/telegram.png',
+    route: '/src/library/telegram.png',
   },
   {
     title: 'marzo',
     category: 'Sticker',
-    route: '/library/marzo.png',
+    route: '/src/library/marzo.png',
   },
   {
     title: 'Comprobacion',
     category: 'Shape',
-    route: '/library/comprobacion.png',
+    route: '/src/library/comprobacion.png',
   }
 ];
 
